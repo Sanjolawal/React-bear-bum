998f2b7c118cd735b08b5489c2cc6f605cd9c137
sanjo lawal <sanjolawal600@gmail.com>
2022-03-08 01:36:09 -0800

    set

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Main.css";
 import Video from "../images/test.gif";
+import Dillus from "../images/D-illus.jpg";
+import eillus from "../images/e-illus.jpg";
+import fillus from "../images/f-illus.jpg";
+import willus from "../images/w-illus.jpg";
+
 
 const Main = () => {
   return (
@@ -26,28 +31,28 @@ const Main = () => {
       <section>
         <h6>PERKS</h6>
         <h1>Welcome To The Crew</h1>
-        <div>
-          <div>
-            <img src="" alt="illus" />
-            <h1>Exclusivity</h1>
+        <div className='firstdiv'>
+          <div className="sectiondiv">
+            <img src={Dillus} alt="llus" />
+            <h3>Exclusivity</h3>
             <p>
               The Bear Bums offers an exclusive 10,000 NFT collection that
               includes extreme rares, animations, and other innovations not yet
               prominent in the NFT space.
             </p>
           </div>
-          <div>
-            <img src="" alt="illus" />
-            <h1>Full Transparency</h1>
+          <div className="sectiondiv">
+            <img src={fillus } alt="illus" />
+            <h3>Full Transparency</h3>
             <p>
               We believe in complete ownership and fair distribution within our
               community. Which is why we have put a strong emphasis on security
               of our investors/community members.
             </p>
           </div>
-          <div>
-            <img src="" alt="illus" />
-            <h1>Tight-knit Community</h1>
+          <div className="sectiondiv">
+            <img src={eillus} alt="illus" />
+            <h3>Tight-knit Community</h3>
             <p>
               Become part of an amazing community that encourages love for one
               another and the planet. As our member base expands our goal is to
@@ -55,9 +60,9 @@ const Main = () => {
               continuous exclusive benefits for being a part of our vision.
             </p>
           </div>
-          <div>
-            <img src="" alt="illus" />
-            <h1>Rewards</h1>
+          <div className="sectiondiv">
+            <img src={willus} alt="illus" />
+            <h3>Rewards</h3>
             <p>
               As the project continues to grow, so do the perks and rewards
               given to our community members. We will be offering consistent
